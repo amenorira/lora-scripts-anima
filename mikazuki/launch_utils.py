9ad876aba42e47d46fc775a44dd366415ab208b6
ameyukisora <64373235+ameyukisora@users.noreply.github.com>
2026-05-20 21:45:33 +0800

    fix: 修复 run() 中 shell 参数的 Pylance 类型报错

--- mikazuki/launch_utils.py
+++ mikazuki/launch_utils.py
@@ -88,14 +88,13 @@ def run(command,
         live: Optional[bool] = True,
         shell: Optional[bool] = None):
 
-    if shell is None:
-        shell = False if sys.platform == "win32" else True
+    use_shell: bool = shell if shell is not None else (False if sys.platform == "win32" else True)
 
     if desc is not None:
         print(desc)
 
     if live:
-        result = subprocess.run(command, shell=shell, env=os.environ if custom_env is None else custom_env)
+        result = subprocess.run(command, shell=use_shell, env=os.environ if custom_env is None else custom_env)
         if result.returncode != 0:
             raise RuntimeError(f"""{errdesc or 'Error running command'}.
 Command: {command}
@@ -104,7 +103,7 @@ Error code: {result.returncode}""")
         return ""
 
     result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                            shell=shell, env=os.environ if custom_env is None else custom_env)
+                            shell=use_shell, env=os.environ if custom_env is None else custom_env)
 
     if result.returncode != 0:
         message = f"""{errdesc or 'Error running command'}.
